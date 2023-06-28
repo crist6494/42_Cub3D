a2616e58f9ac653910e921b705d80a260da5bb62
majiro1996 <majiro1996@gmail.com>
2023-06-28 20:18:58 +0200

    parse wip

--- src/parse/argv_check.c
+++ src/parse/argv_check.c
@@ -6,7 +6,7 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 18:02:00 by manujime          #+#    #+#             */
-/*   Updated: 2023/06/28 20:05:14 by manujime         ###   ########.fr       */
+/*   Updated: 2023/06/28 20:18:16 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,43 +48,30 @@ int	ft_file_check(char *str)
 //checks if the component name and content are in the same line
 //if they are, it saves the content in the components struct
 //and returns 1, else it returns 0
-// int	ft_check_line(char *line, char *comp, t_comp *components)
-// {
-// 	char	*aux;
-// 	int		len;
-
-// 	len = ft_strlen(comp);
-// 	if (ft_strnstr(line, comp, len) != 0)
-// 	{
-// 		aux = ft_strtrim(line, " ");
-// 		if (ft_strcmp(aux, comp) == 0)
-// 		{
-// 			free(aux);
-// 			return (0);
-// 		}
-// 		else
-// 		{
-			
-// 		}
-// 	}
-// 	return (0);
-// }
+int	ft_check_line(char *line, char *comp, t_comp *components)
+{
+	while (line && *line == ' ')
+		line++;
+	if (ft_strnstr(comp, line, ft_strlen(comp)) != 0)
+	{
+		return
+	}
+}
 
 //checks if the file has all the necessary components
 //and saves them in the components struct
-// int	ft_is_valid_file(char *str, t_comp *components)
-// {
-// 	int		fd;
-// 	char	*line;
-// 	char	*aux;
+int	ft_is_valid_file(char *str, t_comp *components)
+{
+	int		fd;
+	char	*line;
 
-// 	fd = open(str, O_RDONLY);
-// 	line = get_next_line(fd);
-// 	while (line)
-// 	{
-// 		if (ft_strnstr("NO SO WE EA FL C", line, 2) != 0)
-// 			aux = ft_strtrim(line, " ");
-// 		free(line);
-// 		line = get_next_line(fd);
-// 	}
-// }
+	fd = open(str, O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (ft_strnstr("NO SO WE EA FL C", line, 2) != 0)
+
+		free(line);
+		line = get_next_line(fd);
+	}
+}
